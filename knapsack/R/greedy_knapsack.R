e12040ffd9664ad7499d3a18b226b8d7983cc515
ZaidaLiendeborg <zaida@liendeborg.se>
2016-10-05 11:28:14 +0200

    greedy 2

--- knapsack/R/greedy_knapsack.R
+++ knapsack/R/greedy_knapsack.R
@@ -3,18 +3,22 @@ greedy_knapsack<- function(x, W){
   weight<- x[,1]
   value<- x[,2]
   r<- numeric(0)
+  
   for (i in 1:nrow(x)){
     r[i]<- value[i]/weight[i]
   }
-  x_sort<- x[order(r,decreasing=FALSE),]
+  x_sort<- x[order(r,decreasing=TRUE),]
   
   items<- numeric(0)
-  for(j in 1:nrow(x_sort)){
-    if (x_sort[j] < W){
-      items[j] <- x_sort[j]
-    }
+  i<- 1
+  cumWeight<- x_sort[i,1]
+  while (cumWeight < W){
+    cumWeight<- cumWeight + x_sort[i:i+1,1]
+    i<- i + 1
   }
+  index<- 
+
   return(items)
 }
 
-greedy_knapsack(x = knapsack_objects[1:800,], W = 3500)
+greedy_knapsack (x = knapsack_objects[1:800,], W = 3500)
