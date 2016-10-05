36baa4b68895a840eba60ab10f2a433a791a5ebd
ZaidaLiendeborg <zaida@liendeborg.se>
2016-10-05 19:24:46 +0200

    for loop istället för while loop

--- knapsack/R/greedy_knapsack.R
+++ knapsack/R/greedy_knapsack.R
@@ -2,23 +2,33 @@
 greedy_knapsack<- function(x, W){
   weight<- x[,1]
   value<- x[,2]
-  r<- numeric(0)
-  
-  for (i in 1:nrow(x)){
-    r[i]<- value[i]/weight[i]
-  }
-  x_sort<- x[order(r,decreasing=TRUE),]
+  r<- value/weight
+  r_sort<- order(r,decreasing=TRUE)
   
   items<- numeric(0)
-  i<- 1
-  cumWeight<- x_sort[i,1]
-  while (cumWeight < W){
-    cumWeight<- cumWeight + x_sort[i:i+1,1]
-    i<- i + 1
+  totWeight<- 0
+  totValue<- 0
+  
+  for (i in r_sort){
+    if ((weight[i] + totWeight) <= W){
+      items<- c(items, i)
+      totWeight<- weight[i] + totWeight
+      totValue<- value[i] + totValue
+    }
+  } 
+  if (totWeight > W){
+    items<- items[1:(length(items)-1)]
   }
-  index<- 
-
-  return(items)
+  
+  ls<- list(value = totValue, elements = items)
+  return(ls)
 }
 
-greedy_knapsack (x = knapsack_objects[1:800,], W = 3500)
+greedy_knapsack(x = knapsack_objects[1:1200,], W = 2000)
+
+i<- 1
+cumWeight<- x_sort[i,1]
+while (cumWeight < W){
+  cumWeight<- cumWeight + x_sort[i:i+1,1]
+  i<- i + 1
+}
